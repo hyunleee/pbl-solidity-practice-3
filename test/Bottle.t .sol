63891f62e9eb3eed82315b506a11c69b24518ca1
Hyunlee <105576592+hyunleee@users.noreply.github.com>
2025-05-09 19:19:07 +0900

    Feat: 개발 세팅

---
+++ test/Bottle.t .sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Bottle} from "../src/Bottle.sol";
+import {CToken} from "../src/CToken.sol";
+
+contract BottleTest is Test {
+    // Counter public counter;
+
+    // function setUp() public {
+    //     counter = new Counter();
+    //     counter.setNumber(0);
+    // }
+
+    // function test_Increment() public {
+    //     counter.increment();w
+    //     assertEq(counter.number(), 1);
+    // }
+
+    // function testFuzz_SetNumber(uint256 x) public {
+    //     counter.setNumber(x);
+    //     assertEq(counter.number(), x);
+    // }
+}
